3f7ce209bd157c46b4daed042925ed5b82278214
Vitalij Merenics <92541517+Vitmer@users.noreply.github.com>
2025-05-03 11:50:15 +0200

    Update deploy workflow and main.tf

--- main.tf
+++ main.tf
@@ -10,6 +10,28 @@ resource "azurerm_resource_group" "rg" {
   location = var.location
 }
 
+resource "azurerm_storage_account" "tfstate" {
+  name                     = "devstorageacctmerenics"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_storage_container" "tfstate" {
+  name                  = "tfstate"
+  storage_account_id    = azurerm_storage_account.tfstate.id
+  container_access_type = "private"
+}
+
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "dev-rg"
+    storage_account_name = "devstorageacctmerenics"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
+}
 # Create an Azure Container Registry
 resource "azurerm_container_registry" "acr" {
   name                = var.acr_name
